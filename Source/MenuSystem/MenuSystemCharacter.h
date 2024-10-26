5eed95771013145c2564c887711032683cb5ff38
robkohout <robkohout@hotmail.com>
2024-10-26 16:58:12 -0700

    Creating a Session

--- Source/MenuSystem/MenuSystemCharacter.h
+++ Source/MenuSystem/MenuSystemCharacter.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "Logging/LogMacros.h"
+#include "Interfaces/OnlineSessionInterface.h"  // Test
 #include "MenuSystemCharacter.generated.h"
 
 class USpringArmComponent;
@@ -73,6 +74,15 @@ public:
 	// Multiplayer Test Code
 public:
 	// Pointer to the online session interface
-	TSharedPtr<class IOnlineSession, ESPMode::ThreadSafe> OnlineSessionInterface;
+	IOnlineSessionPtr OnlineSessionInterface;
+
+protected:
+	UFUNCTION(BlueprintCallable)
+	void CreateGameSession();
+
+	void OnCreateSessionComplete(FName SessionName, bool bWasSuccessful);
+
+private:
+	FOnCreateSessionCompleteDelegate CreateSessionCompleteDelegate;
 };
 
